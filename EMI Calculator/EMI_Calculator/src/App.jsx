9bdfe6ce805c5266bc92ca77b7b2e66948a5efb0
Karanmhaske8604 <karanmhaske3050@gmail.com>
2026-06-15 15:57:00 +0530

    Simple EMI Calculator Project by React

---
+++ EMI Calculator/EMI_Calculator/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import EmiCalculator from './Componenet/EmiCalculator'
+
+function App() {
+  
+  return (
+    <>
+      <EmiCalculator/>
+    </>
+  )
+}
+
+export default App
---
+++ EMI Calculator/EMI_Calculator/src/Componenet/EmiCalculator.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function EmiCalculator()
+{
+    let[amount,setamount]=useState("");
+    let[interest,setinterest]=useState("");
+    let[time,settime]=useState("");
+    let[emi,setemi]=useState(0);
+    let[totalamount,settotalamount]=useState();
+
+    function calculate(){
+
+        let P=Number(amount);
+        let R=Number(interest)/12/100;
+        let N=Number(time)*12;
+
+        const emiValue=(P * R * Math.pow(1+R,N))/(Math.pow(1+R,N)-1);
+        const totalpay=emiValue * N;
+
+        setemi(emiValue.toFixed(2));
+        settotalamount(totalpay.toFixed(2));
+    }
+
+
+    return(<>
+    <h1>EMi-Calculator</h1>
+
+    <input
+        type="number"
+        placeholder="amount"
+        value={amount}
+        onChange={(e)=>setamount(e.target.value)}/>
+        <br />
+    <input
+        type='number'
+        placeholder="interest"
+        value={interest}
+        onChange={(e)=>setinterest(e.target.value)}/>
+        <br/>
+
+    <input
+        type="number"
+        placeholder="time"
+        value={time}
+        onChange={(e)=>settime(e.target.value)}/>
+        <br/>
+
+        <button onClick={calculate}>Emi Calculate</button>
+        <h2>Monthly EMi:{emi}</h2>
+        <h2>TotalAmount:{totalamount}</h2>
+
+    </>);
+}
+export  default EmiCalculator;
